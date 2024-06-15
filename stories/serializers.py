768bfa3f2a8ba403803ba827709a8928a842f58d
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-06-15 17:43:04 +0200

    Adding Django rest auth library and rest framework’s authtoken

--- stories/serializers.py
+++ stories/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
+from likes.serializers import LikeSerializer
+from comments.serializers import CommentSerializer
 from .models import Story
 
 class StorySerializer(serializers.ModelSerializer):
+    likes = LikeSerializer(many=True, read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+
     class Meta:
         model = Story
-        fields = '__all__' 
+        fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at', 'likes', 'comments']
--- stories/views.py
+++ stories/views.py
@@ -1,3 +1,4 @@
+
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
