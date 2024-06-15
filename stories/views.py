3f5175940300307e4029a33164f9bdb3c0a35c3d
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-06-15 15:52:12 +0200

    adding filters to the stoeries

--- stories/views.py
+++ stories/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Story
 from .serializers import StorySerializer
 
@@ -6,6 +8,29 @@ class StoryList(generics.ListCreateAPIView):
     queryset = Story.objects.all()
     serializer_class = StorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    search_fields = [
+        'title',
+        'author__username',
+    ]
+    ordering_fields = [
+        'created_at',
+        'updated_at',
+    ]
+    filterset_fields = {
+        'title': ['icontains'],
+        'author': ['exact'],
+        'created_at': ['gte', 'lte'],
+    }
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
 
 class StoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Story.objects.all()
