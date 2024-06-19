45909a21e8b44d4e6fe673c08d68a5c9678345e4
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-06-19 21:35:31 +0200

    adding '127.0.0.1' to the allowed hosts in setttings.py

--- sheapi/settings.py
+++ sheapi/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost','.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
