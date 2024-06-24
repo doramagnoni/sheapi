d450cd8e7653727aa5a2243477b3cb9e12e16c0c
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-06-24 16:40:25 +0200

    setting debug to debug in os environ

--- sheapi/settings.py
+++ sheapi/settings.py
@@ -63,7 +63,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
