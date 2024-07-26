340715ecee2cecc219f5a35b4d31612fe4b3c9ec
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-07-26 17:33:21 +0200

    adding codeanywhere to the cors variable

--- sheapi/settings.py
+++ sheapi/settings.py
@@ -72,6 +72,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://sheapi-001672ab3b00.herokuapp.com",
     "https://3000-doramagnoni-sheinspires-6xi4izgss9d.ws-eu115.gitpod.io",
     "https://she-inspires-3dbf32b2af97.herokuapp.com"
+    "https://doramagnoni-sheapi-2uygf3suev.us1.codeanyapp.com"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
