d00f200b69311248e57b37317f5a34043a07903d
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-07-27 15:47:28 +0200

    yet again trying to add gitpod to cors

--- sheapi/settings.py
+++ sheapi/settings.py
@@ -70,11 +70,10 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://sheapi-001672ab3b00.herokuapp.com",
-    "https://doramagnoni-sheinspires-f3llen2wk56.ws-eu115.gitpod.io",
-    "https://she-inspires-3dbf32b2af97.herokuapp.com"
-]
+    "https://she-inspires-3dbf32b2af97.herokuapp.com",
+    'https://3000-doramagnoni-sheinspires-f3llen2wk56.ws-eu115.gitpod.io',
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = Truegi
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
